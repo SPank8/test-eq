932c3049f810cb14c2525fe8b40951350438f3cf
Spank8 <spank8@vk.com>
2025-04-15 13:17:15 +0300

    обавлены базовые автотесты и GitHub Actions2

--- tests/test_compare.py
+++ tests/test_compare.py
@@ -4,8 +4,8 @@ from deepdiff import DeepDiff
 def test_compare_responses():
     payload = {"example": "data"}
 
-    ref = requests.post("https://reference-api.example.com/endpoint", json=payload)
-    target = requests.post("https://target-api.example.com/endpoint", json=payload)
+    ref = requests.post("http://webtours.load-test.ru:1080/cgi-bin/nav.pl?in=home", json=payload)
+    target = requests.post("http://webtours.load-test.ru:1090/cgi-bin/nav.pl?in=home", json=payload)
 
     diff = DeepDiff(ref.json(), target.json(), ignore_order=True)
     assert diff == {}, f"Found difference: {diff}"
